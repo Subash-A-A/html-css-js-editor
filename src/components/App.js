0853444d7a15824df6c846d551e8637024454500
Subash <it.subashaa@gmail.com>
2021-12-25 11:16:51 +0530

    editor styling

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
+
+function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
