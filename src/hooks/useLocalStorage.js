d9ec6f3182d75b8345ec93208a6062cb69c341a2
Subash <it.subashaa@gmail.com>
2021-12-29 17:52:44 +0530

    useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,28 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export default function useLocalStorage() {
-  return <div></div>;
+const PREFIX = "editor-";
+
+export default function useLocalStorage(key, initialValue) {
+  const prefixedKey = PREFIX + key;
+
+  const [value, setValue] = useState(() => {
+    console.log(localStorage);
+    const jasonValue = localStorage.getItem(prefixedKey);
+
+    if (jasonValue != null) {
+      return JSON.parse(jasonValue);
+    }
+
+    if (typeof initialValue == "function") {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  return [value, setValue];
 }
